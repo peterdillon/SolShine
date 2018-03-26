5ca6a24182c092e286dde773bcf10263db3d6ea0
Peter Dillon <peter.dillon@evolutionmedcom.com>
2018-03-26 16:50:26 -0400

    A whole bunch of optimizations and addition for even MORE SEO * Added service worker * Meaningful titles for iframes and image links (social links)
    More...

---
+++ sw.js
@@ -0,0 +1,20 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/styles.css'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+
+});
